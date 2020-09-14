d10af7b85eee94d452a14eaa35afd1394084fbfa
Frederikke <rfrederikke@gmail.com>
2020-09-14 11:56:45 +0200

    Projekt

---
+++ js/js.js
@@ -0,0 +1,38 @@
+/*
+ * fil: js.js
+ * purpose: introdction to jQuery
+ */
+console.log('file: js/js.js loaded');
+
+// A $( document ).ready() block.
+$(document).ready(function () { // kører så snart DOM er klar
+
+    console.log("jQuery 3.5.1 running. Alert level green.");
+
+    // ... koden starter her ...
+
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace("active");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        setTimeout(showSlides, 5000); // skifter billede hver femte sekund
+    }
+
+    // ... og slutter her
+
+}); // denne line må ikke slettes
